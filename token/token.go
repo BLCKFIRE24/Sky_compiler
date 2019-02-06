85fe9c728cf68f0901151e5a9c30218bf8739253
Justin <jokerbatman59@gmail.com>
2019-02-06 05:04:07 -0800

    Inital Commit

---
+++ token/token.go
@@ -0,0 +1,40 @@
+package token
+
+type TokenType string
+
+type Token struct {
+
+	Type	TokenType
+	Literal string
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+
+    // Identifiers + literals
+    IDENT = "IDENT" // add, foobar, x, y, ...
+    INT   = "INT"   // 1343456”
+
+	// Operators
+    ASSIGN   = "="
+	PLUS     = "+"
+	
+	// Delimiters
+    COMMA     = ","
+	SEMICOLON = ";"
+	
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//KEYWORDS
+	FUNCTION = "FUNCTION"
+	Var = "var"
+)
+    
+
+	
+	
+
